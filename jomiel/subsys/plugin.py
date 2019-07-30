0e3c5896452254f8eb7bdaf5454f05fd66c754b4
Toni Gündoğdu <47444442+guendto@users.noreply.github.com>
2019-07-30 15:22:57 +0300

    subsys/plugin: add module

---
+++ jomiel/subsys/plugin.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+#
+# jomiel
+#
+# Copyright
+#  2019 Toni Gündoğdu
+#
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+"""TODO."""
+
+
+def init():
+    """Initiates the plugin subsystem."""
+
+    from jomiel.plugin import load
+    import jomiel.cache as cache
+    from jomiel import lg
+
+    def log(text):
+        """Write a new (debug) entry to the logger."""
+        lg().debug('subsystem/plugin: %s', text)
+
+    cache.plugin_packages = {}
+    cache.plugin_handlers = {}
+
+    from importlib import import_module
+    namespace_packages = [import_module('jomiel.plugin.media')]
+
+    for ns_pkg in namespace_packages:
+        ns_name = ns_pkg.__name__
+
+        cache.plugin_packages[ns_pkg] = load(ns_pkg)
+        cache.plugin_handlers[ns_name] = []
+
+        for pkg_name in cache.plugin_packages[ns_pkg]:
+            module = cache.plugin_packages[ns_pkg][pkg_name]
+            handler = getattr(module, 'Handler')()
+
+            cache.plugin_handlers[ns_name].append(handler)
+            log('<%s> loaded %s' % (pkg_name, ns_pkg))
+
+        num_handlers = len(cache.plugin_handlers[ns_name])
+        log('<%s> cached %d handler(s)' % (ns_name, num_handlers))
+
+    no_packages = len(cache.plugin_packages)
+    log('cached %d package(s)' % no_packages)
+
+    lg().info('plugin subsystem initiated')
+
+    from jomiel.cache import opts, dump_plugins  # pylint: disable=E0611
+    if opts.plugin_list:
+        dump_plugins()
+
+
+# vim: set ts=4 sw=4 tw=72 expandtab:
