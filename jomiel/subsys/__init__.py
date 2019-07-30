47e6a893068eebcc737f3fb585341d9451c28624
Toni Gündoğdu <47444442+guendto@users.noreply.github.com>
2019-07-30 15:22:09 +0300

    jomiel/subsys: add module

---
+++ jomiel/subsys/__init__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+#
+# jomiel
+#
+# Copyright
+#  2019 Toni Gündoğdu
+#
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+"""TODO."""
+
+
+def init():
+    """Initiates the application subsystems."""
+    from jomiel.subsys import log, hypertext, plugin, broker
+
+    log.init()
+    plugin.init()
+    hypertext.init()
+    broker.init()
+
+    from jomiel import lg
+    lg().info('exit normally')
+
+
+# vim: set ts=4 sw=4 tw=72 expandtab:
