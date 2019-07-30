28ddda0f469b6c1c4f80c3f84c063d56c8656523
Toni Gündoğdu <47444442+guendto@users.noreply.github.com>
2019-07-30 15:18:36 +0300

    plugin/media/parser: add module

---
+++ jomiel/plugin/media/parser.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+#
+# jomiel
+#
+# Copyright
+#  2019 Toni Gündoğdu
+#
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+"""TODO."""
+
+from abc import ABCMeta, abstractmethod
+
+
+class PluginMediaParser:
+    """An abstract plugin media parser class.
+
+    Notes:
+        - Subclass this when you write a new media parser
+
+    Attributes:
+        response (obj): the Response (protobuf) message object to be
+            returned to the client
+
+        media (obj): a short-hand for response.media
+    """
+    __slots__ = ['response', 'media']
+    __metaclass__ = ABCMeta
+
+    def __init__(self, uri_components):  # pylint: disable=unused-argument
+        """Initializes the object.
+
+        Args:
+            uri_components (dict): The parsed URI componenets
+
+        """
+        from jomiel.subsys.broker.worker import ResponseBuilder
+        (self.response, self.media) = (None, None)
+
+        self.response = ResponseBuilder().response
+        self.media = self.response.media  # pylint: disable=E1101
+
+    @abstractmethod
+    def parse(self, uri_components):
+        """[Override] Parses the relevant metadata for the media.
+
+        Args:
+            uri_components (dict): The input URI components
+
+        """
+    def __repr__(self):
+        """Returns an unambiguous representation of the object."""
+        return "Media:\n  response=%s" % self.response
+
+
+# vim: set ts=4 sw=4 tw=72 expandtab:
