a87561211217cbc20d387ac92670ec8011930a12
Toni Gündoğdu <47444442+guendto@users.noreply.github.com>
2019-07-30 15:22:57 +0300

    subsys/hypertext: add module

---
+++ jomiel/subsys/hypertext.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+#
+# jomiel
+#
+# Copyright
+#  2019 Toni Gündoğdu
+#
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+"""TODO."""
+
+
+def init():
+    """Initiates the HTTP subsystem."""
+
+    from jomiel.cache import opts  # pylint: disable=E0611
+    from jomiel import lg
+
+    if opts.http_debug:
+        from jomiel.hypertext import be_verbose
+        be_verbose()
+        lg().debug('enable http logging')
+
+    lg().info('http subsystem initiated')
+
+
+# vim: set ts=4 sw=4 tw=72 expandtab:
