41a5e93867cf827d5387f85285dbcfbe82339d87
Toni Gündoğdu <47444442+guendto@users.noreply.github.com>
2019-11-04 13:57:03 +0200

    keygen/app: make style changes according to "black" code formatter

--- jomiel/keygen/app.py
+++ jomiel/keygen/app.py
@@ -19,18 +19,20 @@ class App(KoreApp):
     __slots__ = []
 
     def version_long_modules(self):
-        return {'zmq': 'PyZMQ'}
+        return {"zmq": "PyZMQ"}
 
     def run(self):
         """Application entry point; executes the app."""
         from configargparse import get_parser
+
         parser = get_parser()
 
         parser.add(
-            'filename',
-            help='''Name for the authentication key-pair files for use
-                    with jomiel''',
-            nargs='+')
+            "filename",
+            help="""Name for the authentication key-pair files for use
+                    with jomiel""",
+            nargs="+",
+        )
 
         opts = super(App, self).parse_opts(parser)
 
@@ -46,19 +48,25 @@ class App(KoreApp):
 
             try:
                 name = basename(fpath)
-                (public_key_file,
-                 secret_key_file) = create_certificates(basedir, name)
+                (
+                    public_key_file,
+                    secret_key_file,
+                ) = create_certificates(basedir, name)
             except IOError as message:
-                print('error: %s' % message)
+                print("error: %s" % message)
                 from jomiel.kore.app import exit_error
+
                 exit_error()
 
             from os import chmod
+
             chmod(public_key_file, 0o644)
             chmod(secret_key_file, 0o400)
 
-            print('Created:\n  %s\n  %s' %
-                  (public_key_file, secret_key_file))
+            print(
+                "Created:\n  %s\n  %s"
+                % (public_key_file, secret_key_file)
+            )
 
         for filename in opts.filename:
             generate_keypair(filename)
