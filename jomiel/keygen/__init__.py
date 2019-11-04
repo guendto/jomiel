af94e7ff53d7636248399db8a175d1a9a289d3f7
Toni Gündoğdu <47444442+guendto@users.noreply.github.com>
2019-11-04 13:56:11 +0200

    keygen/__init__.py: make style changes according to "black" code formatter

--- jomiel/keygen/__init__.py
+++ jomiel/keygen/__init__.py
@@ -14,16 +14,18 @@
 def main():
     """main"""
     from sys import path
-    path.insert(0, '.')
+
+    path.insert(0, ".")
 
     from jomiel.keygen.app import App
 
     App(
         no_default_config_files=True,
-        pkg_resources_name='jomiel',  # shares the pkg_resources
+        pkg_resources_name="jomiel",  # shares the pkg_resources
         no_config_file_option=True,
         no_logger_options=True,
-        no_print_config=True).run()
+        no_print_config=True,
+    ).run()
 
 
 # vim: set ts=4 sw=4 tw=72 expandtab:
