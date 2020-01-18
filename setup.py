fb2e37f1bf168556be5cea60b923682faa13d247
Toni Gündoğdu <47444442+guendto@users.noreply.github.com>
2020-01-18 16:28:39 +0200

    setup.py: read package names from requirements.in file into a list

--- setup.py
+++ setup.py
@@ -43,6 +43,8 @@ from jomiel.kore.setup.cmd import CustomCommand__clean
 from jomiel.kore.setup.version import get_semantic_version
 from jomiel.kore.setup.file import read_file
 
+requirements = read_file("requirements.in").splitlines()
+
 # setup()
 #
 from setuptools import setup, find_namespace_packages
