b46cf4690b743da7d5003811adba031e8ac35689
Toni Gündoğdu <47444442+guendto@users.noreply.github.com>
2020-01-18 15:44:37 +0200

    jomiel/__main__.py: rewrite replacing __init__.py, making "jomiel" a ns
    - jomiel/ has become a namespace (thus, __init__.py was removed)
      - __main__.py replaces __init__.py completely
    
    * We are using the "jomiel" namespace with the protobuf bindings
    generated from the jomiel/comm/proto/**/*.proto files.

--- jomiel/__main__.py
+++ jomiel/__main__.py
@@ -3,20 +3,39 @@
 # jomiel
 #
 # Copyright
-#  2019 Toni Gündoğdu
+#  2019-2020 Toni Gündoğdu
 #
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 """TODO."""
 
-if __name__ == "__main__":
+
+def main():
+    """main"""
     from sys import path
 
-    path.insert(0, ".")
+    path.append(".")
 
-    from jomiel import main
+    from jomiel.app import App
 
-    main()
+    pkg_name = "jomiel"
+    data_dir = "%s.data" % pkg_name
+
+    App(
+        package_name=pkg_name,
+        package_data_dir=data_dir,
+        package_additional_search_paths=["%s.bindings" % data_dir],
+        config_module="%s.cache" % pkg_name,
+    ).run()
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except ModuleNotFoundError as error:
+        from jomiel.kore.error import if_proto_bindings_missing
+
+        if_proto_bindings_missing(error)
 
 # vim: set ts=4 sw=4 tw=72 expandtab:
