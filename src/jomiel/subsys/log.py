d2cf98ec51d00a06e6cac85b07c257df731fee23
Toni Gündoğdu <47444442+guendto@users.noreply.github.com>
2021-10-06 20:19:48 +0300

    feat: notify if --logger-ident did not match the logger identities
    Call the `jomiel_kore.log.log_init` function (added in v0.2.2) with the
    `ident_must_exist` kwarg. The program will now exit with an error if the
    value matched nothing.

--- src/jomiel/subsys/log.py
+++ src/jomiel/subsys/log.py
@@ -15,7 +15,10 @@ def init():
     from jomiel.cache import logger_paths, opts
     from jomiel_kore.log import log_init
 
-    (logger_file, idents) = log_init(logger_paths)
+    (logger_file, idents) = log_init(
+        logger_paths,
+        ident_must_exist=opts.logger_ident,
+    )
 
     from jomiel.log import lg
 
